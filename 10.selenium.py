de7096640a0bd2fc4c033e72acc7eb44c612181e
전봉근 <n84066@gmail.com>
2025-06-13 19:38:16 +0900

    크롤링 아놀자까지 완

--- 10.selenium.py
+++ 10.selenium.py
@@ -1,32 +1,27 @@
-# 네이버 웹툰 댓글 크롤링
-# https://comic.naver.com/webtoon/detail?titleId=702672&no=1&week=finish
+# 네이버 웹툰 크롤링
+
 import time
 from selenium import webdriver
-# python -m pip install bs4
 from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome()
-driver.get('https://comic.naver.com/webtoon/detail?titleId=702672&no=1&week=finish')
+driver.get('https://comic.naver.com/webtoon/detail?titleId=783053&no=134&week=tue')
 soup = BeautifulSoup(driver.page_source)
-comment_area = soup.find_all('span',{'classs':'u_cbox_contents'})
-print('******* 베스트 댓글 *******')
+comment_area = soup.findAll('span', {'class', 'u_cbox_contents'})
+print('********** 베스트 댓글 **********')
 for i in range(len(comment_area)):
-  comment = comment_area[i].text.strip()
-  print(comment)
-  print('-'*30)
+    comment = comment_area[i].text.strip()
+    print(comment)
+    print('-' * 30)
 
-# /html/body/div[1]/div[5]/div/div/div[4]/div[1]/div[3]/div/div/div[4]/div[1]/div/ul/li[2]/a/span[2]
-# 전체댓글 클릭릭
-driver.find_element('xpath','/html/body/div[1]/div[5]/div/div/div[4]/div[1]/div[3]/div/div/div[4]/div[1]/div/ul/li[2]/a/span[2]').click()
-time.sleep(2) # 클릭후 불러오기전 아래코드가 실행되는것을 방지지
+driver.find_element('xpath', '//*[@id="cbox_module_wai_u_cbox_sort_option_tab2"]/span[2]').click()
+time.sleep(2)
 
-soup = BeautifulSoup(driver.page_source) #다시 페이지 소스 들고오기기
-comment_area = soup.find_all('span',{'classs':'u_cbox_contents'})
-print('******* 전체 댓글 *******')
+soup = BeautifulSoup(driver.page_source)
+comment_area = soup.findAll('span', {'class', 'u_cbox_contents'})
+
+print('********** 전체 댓글 **********')
 for i in range(len(comment_area)):
-  comment = comment_area[i].text.strip()
-  print(comment)
-  print('-'*30)
-
-time.sleep(10)
-
+    comment = comment_area[i].text.strip()
+    print(comment)
+    print('-' * 30)
--- 11.selenium.py
+++ 11.selenium.py
@@ -3,23 +3,43 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 def crawl_yanolja_reviews(name, url):
-  review_list = []
-  driver = webdriver.Chrome()
-  driver.get(url)
-  time.sleep(2)
-
-  scroll_count = 3
-  for i in range(scroll_count):
-    driver.excute_script('window.scrollTo(0, document.body.scrollHeight)')
+    review_list = []
+    driver = webdriver.Chrome()
+    driver.get(url)
     time.sleep(2)
-  soup = BeautifulSoup(driver.page_source)
 
-  review_containers = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div')
-  review_date = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div > div.css-1toaz2b > div > div.css-1ivchjf > p')
-  
-  for i in range(len(review_containers)):
-    print(i)
-  
-  time.sleep(10)
+    scroll_count = 3
+    for i in range(scroll_count):
+        # driver.execute_script : 자바 스크립트 코드를 사용하게 해주는 역할(스크롤을 올려라)
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+        time.sleep(2)
+
+    soup = BeautifulSoup(driver.page_source)
+
+    # 댓글 박스부분을 패턴으로 검색(개발자 도구에서 select 복사)
+    review_containers = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div')
+    # 작성일 select 부분코드
+    review_date = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div > div.css-1toaz2b > div > div.css-1ivchjf > p')
+    
+
+    for i in range(len(review_containers)):
+        # find() 로 불러올때 class 속성은 _ 를 붙어야 한다
+        review_text = review_containers[i].find('p', class_='content-text').text
+        date = review_date[i].text
+        # print(review_text, date)
+        review_empty_stars = review_containers[i].find_all('path', {'fill-rule':'evenodd'})
+        # print(review_empty_stars)
+        stars = 5 - len(review_empty_stars)
+        # review 내용을 dict형식으로 list에 저장
+        review_dict = {
+          'review': review_text,
+          'date': date,
+          'stars' : stars
+        }
+        review_list.append(review_dict)
+
+    print(review_list)
+
+    time.sleep(10)
 
 crawl_yanolja_reviews('신라스테이 여수', 'https://www.yanolja.com/reviews/domestic/10046614')
